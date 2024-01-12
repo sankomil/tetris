7286a5576ae6d7638422bcc7ef3fd6f72a25a226
sankomil <43489998+sankomil@users.noreply.github.com>
2024-01-12 16:29:55 +0800

    put all global vars in 1 place

---
+++ globals.py
@@ -0,0 +1,8 @@
+screen_width = 800
+screen_height = 700
+play_width = 300
+play_height = 600 
+block_size = 30
+
+top_left_x = (screen_width - play_width) // 2
+top_left_y = screen_height - play_height
